5f2940ebc4e7df28e288fb312c1c7f4885aef8e1
madhurgang <mdhrgngwn111@gmail.com>
2025-04-13 14:39:11 +0530

    unused imports removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { QRCodeCanvas } from 'qrcode.react';
 import { v4 as uuidv4 } from 'uuid';
 
